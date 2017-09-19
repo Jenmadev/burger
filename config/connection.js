dceebef701ad7e9680892efb57a91922e3486d3e
Jenmadev <info@maknowsthat.com>
2017-09-18 17:42:13 -0700

    connection.js connection updated

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,23 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db'
-  });
-  connection.connect((err) => {
+    if process.env.JAWSDB_URL){
+        connection = mysql.createConnection(process.env.JAWSDB_URL);
+    }
+    else{
+        connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'burgers_db'
+    });
+
+  
+  
+  
+    connection.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
   });
 
-  module.exports = mysql;
+  module.exports = connection;
